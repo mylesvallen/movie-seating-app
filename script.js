4fb7b3d3391224f5c8626d91c34ced5e55c80c50
Myles Allen <mylesvallen@icloud.com>
2020-03-25 21:32:30 -0700

    js file

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+
+const ticketPrice = +movieSelect.value;
