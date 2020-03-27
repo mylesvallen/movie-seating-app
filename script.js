3bfb527c00a5dcbbd27d88d0f5eb30241aa75064
Myles Allen <mylesvallen@icloud.com>
2020-03-26 19:24:45 -0700

    settings

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied)");
-const count = document.getElementById("count");
-const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
 const ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
-	const selectedSeats = document.querySelectorAll(".row .seat.selected");
+	const selectedSeats = document.querySelectorAll('.row .seat.selected');
 }
 
 //click event on the container that holds the seats | 2nd parameter is the fuction
-container.addEventListener("click", e => {
+container.addEventListener('click', e => {
 	if (
-		e.target.classList.contains("seat") &&
-		!e.target.classList.contains("occupied")
+		e.target.classList.contains('seat') &&
+		!e.target.classList.contains('occupied')
 	) {
-		e.target.classList.toggle("selected");
+		e.target.classList.toggle('selected');
 	}
 });
