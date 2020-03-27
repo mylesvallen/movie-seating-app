6f3f7ca227068b9b7abbb8abda2193453af2818e
Myles Allen <mylesvallen@icloud.com>
2020-03-26 19:07:45 -0700

    a

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-
 const ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+	const selectedSeats = document.querySelectorAll(".row .seat.selected");
 }
 
 //click event on the container that holds the seats | 2nd parameter is the fuction
-container.addEventListener("click", (e) => {
-    if (
-        e.target.classList.contains("seat") &&
-        !e.target.classList.contains("occupied")
-    ) {
-        e.target.classList.toggle("selected");
-    }
+container.addEventListener("click", e => {
+	if (
+		e.target.classList.contains("seat") &&
+		!e.target.classList.contains("occupied")
+	) {
+		e.target.classList.toggle("selected");
+	}
 });
