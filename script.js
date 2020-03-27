6b7c3a9645b998567dd99e018ea03061d3385976
Myles Allen <mylesvallen@icloud.com>
2020-03-26 18:54:40 -0700

    js file

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 const ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+}
+
+//click event on the container that holds the seats | 2nd parameter is the fuction
+container.addEventListener("click", (e) => {
+    if (
+        e.target.classList.contains("seat") &&
+        !e.target.classList.contains("occupied")
+    ) {
+        e.target.classList.toggle("selected");
+    }
+});
